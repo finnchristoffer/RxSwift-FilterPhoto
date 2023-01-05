c57f6de925ea0ba66469eaf5ff35969750e6f94f
finnchristoffer <finn.christoffer@gmail.com>
2023-01-05 12:05:04 +0700

    RxSwift: Observeable, subscribe

---
+++ CameraFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilter
+//
+//  Created by Finn Christoffer Kurniawan on 04/01/23.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
